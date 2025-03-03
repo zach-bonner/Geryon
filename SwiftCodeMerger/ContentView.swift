9341d0c33d677ae1b9fffcc71d8721453652fadd
zach-bonner <88410679+zach-bonner@users.noreply.github.com>
2025-03-02 17:27:25 -0800

    Initial Commit

---
+++ SwiftCodeMerger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCodeMerger
+//
+//  Created by Zachary Bonner on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCodeMerger/SwiftCodeMergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCodeMergerApp.swift
+//  SwiftCodeMerger
+//
+//  Created by Zachary Bonner on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCodeMergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
